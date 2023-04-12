0a2aee069e87a03b2997c4164dc57021ba65a9b9
Thomerson Roncally Araújo Teixeira <thomersonroncally@outlook.com>
2023-04-12 00:26:49 -0400

    feature: Add Mason and reorganize plugins

--- .config/nvim/lua/t7n/packer.lua
+++ .config/nvim/lua/t7n/packer.lua
@@ -4,56 +4,71 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    
-    -- Carbon Theme
-    use 'michaeldyrynda/carbon.vim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    use 'kyazdani42/nvim-web-devicons'
+  -- Carbon Theme
+  use 'michaeldyrynda/carbon.vim'
 
-    -- Lualine
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  -- Lualine
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 
+      'kyazdani42/nvim-web-devicons', 
+      opt = true 
     }
-    
-    -- Nvim LSP
-    use 'neovim/nvim-lspconfig'
+  }
 
-    -- Telescope
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { {'nvim-lua/plenary.nvim'} }
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.x',
+    requires = { 
+      'nvim-lua/plenary.nvim'
     }
+  }
 
-    -- Telescope: FZF Extension
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  -- Telescope: FZF Extension
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
-    -- Treesitter
-    use {
-      'nvim-treesitter/nvim-treesitter',
-      run = function()
-        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-        ts_update()
-      end,
+  -- LSP
+  use {
+    'neovim/nvim-lspconfig',
+    requires = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim'
     }
-    use 'nvim-treesitter/playground'
-    use 'nvim-treesitter/nvim-treesitter-context'
+  }
 
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/nvim-cmp'
+  -- Autocompletion
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip'
+    }
+  }
 
-    use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
 
-    use 'nvim-lua/plenary.nvim'
-    use 'roncallyt/vrello.nvim'
-    use 'ThePrimeagen/harpoon'
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+  use 'nvim-treesitter/playground'
+  use 'nvim-treesitter/nvim-treesitter-context'
 
-    use 'github/copilot.vim'
+  use 'nvim-lua/plenary.nvim'
+  use 'roncallyt/vrello.nvim'
+  use 'ThePrimeagen/harpoon'
 
-    use 'tpope/vim-fugitive'
+  use 'github/copilot.vim'
+
+  use 'tpope/vim-fugitive'
 end)
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -119,6 +119,16 @@ _G.packer_plugins = {
     path = "/home/t7n/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  ["mason-lspconfig.nvim"] = {
+    loaded = true,
+    path = "/home/t7n/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
+  ["mason.nvim"] = {
+    loaded = true,
+    path = "/home/t7n/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
+  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "/home/t7n/.local/share/nvim/site/pack/packer/start/nvim-cmp",
@@ -140,8 +150,9 @@ _G.packer_plugins = {
     url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
   },
   ["nvim-web-devicons"] = {
-    loaded = true,
-    path = "/home/t7n/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    loaded = false,
+    needs_bufread = false,
+    path = "/home/t7n/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
